6e6503327aaa9c3293cb4cea3b927f9c0f605054
tuyizereerneste <tuyizereernest@yahoo.com>
2024-05-15 17:17:29 +0300

    tests cases

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = Router();
 
